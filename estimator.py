515a30afad702ac0862f730b99c56d651b500a8f
Timothé Jobin <timoj@MacBook-Air-37.local>
2026-02-02 17:26:53 +0100

    improved comments in estimator.py

--- estimator.py
+++ estimator.py
@@ -2,17 +2,33 @@ from qiskit_aer import AerSimulator
 from qiskit_aer.noise import NoiseModel, depolarizing_error
 from qiskit_aer.primitives import EstimatorV2
 from qiskit.primitives import StatevectorEstimator
-import numpy as np
 
 
-def get_estimator(nqubits, estimator_name, n_shots, p_err_1q = 0.001, p_err_2q = 0.02):
+def get_estimator(
+        nqubits: int = 6,
+        estimator_name: str = 'noisy',
+        n_shots: int = 0, # Not useful in this project, see note about it below
+        p_err_1q: float = 0.001,
+        p_err_2q: float = 0.02
+        ):
+
+    """
+    Returns a qiskit Estimator instance
+        Args:
+            - nqubits: int, number of qubits of the system
+            - estimator_name: str, name of the type of qiskit Estimator; either "noiseless" for the exact 
+                StatevectorEstimator or "noisy" for the noisy AerEstimator
+            - p_err_1q: float, single-qubit depolarizing error probability
+            - p_err_2q: float, two-qubit depolarizing error probability
+        Returns:
+            - estimator: qiskit.primitives.EstimatorV2 or qiskit_aer.primitives.StatevectorEstimator instance
+    """
 
     if estimator_name == 'noiseless':
         estimator = StatevectorEstimator()
 
     elif estimator_name == 'noisy':
 
-        # --- 1. SETUP NOISY ESTIMATOR V2 ---
         # Define Noise Model
         noise_model = NoiseModel(basis_gates=['id', 'rz', 'sx', 'x', 'cx'])
 
@@ -20,8 +36,7 @@ def get_estimator(nqubits, estimator_name, n_shots, p_err_1q = 0.001, p_err_2q =
         noise_model.add_all_qubit_quantum_error(depolarizing_error(p_err_1q, 1), ["u1", "u2", "u3", "rz", "sx", "x"])
         noise_model.add_all_qubit_quantum_error(depolarizing_error(p_err_2q, 2), ["cx"])
         
-        # Define Connectivity (Coupling Map)
-        # Essential for valid noise simulation!
+        # Defines the qubit connectivity (here, Heavy-Hex) for 4 or 6 qubits
         if nqubits == 6:
             coupling_map = [
                 [0, 1], [1, 0],
@@ -29,30 +44,19 @@ def get_estimator(nqubits, estimator_name, n_shots, p_err_1q = 0.001, p_err_2q =
                 [2, 3], [3, 2],
                 [3, 4], [4, 3],
                 [4, 5], [5, 4],
-                [5, 0], [0, 5]  # Closes the ring
+                [5, 0], [0, 5]
             ]
-        else:
+        elif nqubits ==4:
             coupling_map = [
                 [0, 1], [1, 0],
                 [1, 2], [2, 1],
-                [2, 3], [3, 2]
+                [2, 3], [3, 2],
+                [2, 0], [0, 2]
             ]
-        
-        # Define your configuration dict
-#         options = {
-#             "backend_options": {
-#                 "noise_model": noise_model,
-#                 "coupling_map": coupling_map,  # Optional but recommended with noise
-#                 "basis_gates": noise_model.basis_gates
-#             },
-#             "run_options": {
-#                 "seed": 42,
-#                 "shots": n_shots
-#             }
-# }
-#         estimator = EstimatorV2(options=options)
-
-        # 2. Instantiate the Simulator Backend FIRST (The "Solid Source")
+        else:
+            raise ValueError("Number of qubits not supported; must be either 4 or 6")
+       
+        # Instantiate the simulator backend
         # We pass the noise model directly to the backend class.
         backend = AerSimulator(
             noise_model=noise_model,
@@ -60,12 +64,13 @@ def get_estimator(nqubits, estimator_name, n_shots, p_err_1q = 0.001, p_err_2q =
             basis_gates=noise_model.basis_gates
         )
 
-        # 3. Create Estimator from the Backend
+        # Create Estimator from the Backend
         # This automatically configures the estimator to use the noisy backend.
         estimator = EstimatorV2.from_backend(backend)
 
-        # 4. Set Shot Noise (The "default_shots" is correct, but we set it on the object)
-        # This overrides the default_precision=0.0 and enables shot noise.
-        estimator.options.default_precision =  0 # 1 / np.sqrt(n_shots)
+        # Set shot noise via default_precision setting
+        # Note : here, shot noise is essentially set to 0 because the AerEstimator instance adds it
+        # as posterior Gaussian noise, which is hardware-independent.
+        estimator.options.default_precision =  0    # 1 / np.sqrt(n_shots)
         estimator.options.seed_simulator = 0
     return estimator
